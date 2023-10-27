cb83950097c8666e05373caff5a6bf532baccc20
Peter Akhigbe <akhigbe37@gmail.com>
2023-10-27 20:16:32 +0100

    Basic Informational Site

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.use('/', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.listen(8080, () => {
+  console.log('server listening on port 8080');
+});
+
+console.log(__dirname);
